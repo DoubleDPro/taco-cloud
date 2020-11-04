5558c50eebc1e91f87e9b44f514d5f611f5b6553
Dmitry <vizant88@gmail.com>
2020-11-04 16:36:46 +0300

    added h2 db

--- src/test/java/sia/tacocloud/tacos/HomeControllerTest.java
+++ src/test/java/sia/tacocloud/tacos/HomeControllerTest.java
@@ -12,7 +12,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(HomeController.class)
 public class HomeControllerTest {
 
     @Autowired
